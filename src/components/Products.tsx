63c8f5df9862b83ce5d5e4d13fcae3c48c5df606
Adam Crowley <adamcrowley@gmail.com>
2025-01-30 11:32:51 +1300

    Add grid for products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,9 +4,11 @@ function Products({ productData }: ProductData) {
   return (
     <div className="products__grid">
       {productData.map((product: Product) => (
-        <div className="products__grid-item">
-          <img src={product.image.thumbnail} alt={product.name} />
-          <button>Add to cart</button>
+        <div className="product">
+          <div className="product__img-container">
+            <img src={product.image.desktop} alt={product.name} />
+            <button className="product__button">Add to cart</button>
+          </div>
           <p>{product.category}</p>
           <p>{product.name}</p>
           <p>{product.price}</p>
