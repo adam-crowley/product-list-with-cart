cb3a3b58ab12d733fb7ea1607a641285f93265b4
Adam Crowley <adamcrowley@gmail.com>
2025-02-12 11:32:39 +1300

    Add 'click background to close' function to Cart modal window

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -113,71 +113,70 @@ function Cart({
         </div>
       </aside>
       <dialog open={isModalOpen} className="dialog">
-        <div className="dialog__bg">
-          <div className="dialog__content">
-            <img
-              className="dialog__confirm"
-              src="../../assets/images/icon-order-confirmed.svg"
-              alt=""
-            />
-            <h2>Order Confirmed</h2>
-            <p>We hope you enjoy your food!</p>
-            <div className="dialog__table-wrapper">
-              <table className="dialog__table">
-                <tbody>
-                  {cart.map((product: Product) => (
-                    <tr key={product.id}>
-                      <td className="dialog__thumb-td">
-                        <img
-                          className="dialog__thumb-img"
-                          src={product.image.thumbnail}
-                          alt={product.name}
-                        />
-                      </td>
-                      <td>
-                        <span className="dialog__title">{product.name}</span>
-                        <br></br>
-                        <span className="dialog__volume">{product.qty}x</span>
-                        <span className="dialog__price">
-                          @ ${displayDecimal(product.price)}
-                        </span>
-                      </td>
-                      <td className="dialog__total-td">
-                        <span className="dialog__total">
-                          ${displayDecimal(product.price * (product.qty ?? 0))}
-                        </span>
-                      </td>
-                    </tr>
-                  ))}
-
-                  <tr>
-                    <td colSpan={2}>Order Total</td>
-                    <td className="dialog__order-total">
-                      $
-                      {displayDecimal(
-                        cart.reduce(
-                          (accumulator, product: Product) =>
-                            accumulator + product.price * (product.qty ?? 0),
-                          0
-                        )
-                      )}
+        <div onClick={() => setIsModalOpen(false)} className="dialog__bg"></div>
+        <div className="dialog__content">
+          <img
+            className="dialog__confirm"
+            src="../../assets/images/icon-order-confirmed.svg"
+            alt="Order confimed icon"
+          />
+          <h2>Order Confirmed</h2>
+          <p>We hope you enjoy your food!</p>
+          <div className="dialog__table-wrapper">
+            <table className="dialog__table">
+              <tbody>
+                {cart.map((product: Product) => (
+                  <tr key={product.id}>
+                    <td className="dialog__thumb-td">
+                      <img
+                        className="dialog__thumb-img"
+                        src={product.image.thumbnail}
+                        alt={product.name}
+                      />
+                    </td>
+                    <td>
+                      <span className="dialog__title">{product.name}</span>
+                      <br></br>
+                      <span className="dialog__volume">{product.qty}x</span>
+                      <span className="dialog__price">
+                        @ ${displayDecimal(product.price)}
+                      </span>
+                    </td>
+                    <td className="dialog__total-td">
+                      <span className="dialog__total">
+                        ${displayDecimal(product.price * (product.qty ?? 0))}
+                      </span>
                     </td>
                   </tr>
-                </tbody>
-              </table>
-            </div>
-            <form method="dialog">
-              <button
-                className="dialog__confirm-btn"
-                onClick={() => {
-                  setIsModalOpen(false)
-                  clearCart()
-                }}
-              >
-                Start New Order
-              </button>
-            </form>
+                ))}
+
+                <tr>
+                  <td colSpan={2}>Order Total</td>
+                  <td className="dialog__order-total">
+                    $
+                    {displayDecimal(
+                      cart.reduce(
+                        (accumulator, product: Product) =>
+                          accumulator + product.price * (product.qty ?? 0),
+                        0
+                      )
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+          <form method="dialog">
+            <button
+              className="dialog__confirm-btn"
+              onClick={() => {
+                setIsModalOpen(false)
+                clearCart()
+              }}
+            >
+              Start New Order
+            </button>
+          </form>
         </div>
       </dialog>
     </>
